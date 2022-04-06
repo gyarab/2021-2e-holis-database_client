f80cfb8a395a71090c41f864210947ed62f0b7bf
oren.holis <oren.holis@bronet.cz>
2022-04-06 21:53:44 +0200

    Updated form design

--- code/python/widgets/form.py
+++ code/python/widgets/form.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 
+
 class FormButton:
 	def __init__(self, name, command):
 		self.name = name
@@ -19,13 +20,16 @@ class Field:
 class Form:
 	def __init__(self, root, title, fields, buttons):
 		self.fields = fields
-		self.main_frame = Frame(root)
+		self.main_frame = Frame(root, width=400)
+		self.form_frame = Frame(self.main_frame, bg="white", height=3, relief=RAISED, padx=10)
 		self.entries = []
 		self.buttons = buttons
 
-		Label(self.main_frame, text=title)
+		Label(self.form_frame, bg="white", text=title, height=2, pady=1, font=('Arial', 25, 'bold'), justify=CENTER).pack()
 		self.show_fields()
 		self.show_buttons()
+		self.form_frame.pack(fill=X, expand=True)
+		self.form_frame.place(relx=0.2, rely=0.2)
 
 	def get_values(self):
 		vals = []
@@ -42,8 +46,10 @@ class Form:
 		return vals
 
 	def show_fields(self):
+		i = 0
 		for f in self.fields:
-			frame = Frame(self.main_frame)
+			i = i + 1
+			frame = Frame(self.form_frame, bg="white")
 			field = None
 
 			if f.type == "check":
@@ -51,16 +57,19 @@ class Form:
 				Checkbutton(frame, text=f.text, variable=var).pack()
 				field = var
 			else:
-				Label(frame, text=f.text).pack()
-				field = Entry(frame)
+				Label(frame, text=f.text, background="white", font=('Arial', 12, 'bold')).pack()
+				field = Entry(frame, justify=CENTER, width=20, border=0)
+
+				if i == 1:
+					field.focus()
 
 				if len(f.value) > 0:
 					field.insert(0, f.value)
 
-				field.pack()
+				field.pack(pady=5)
 
 			if len(f.warning) > 0:
-				Label(frame, text=f.warning).pack()
+				Label(frame, text=f.warning, background="white", wraplength=360).pack()
 
 			self.entries.append(field)
 
@@ -68,4 +77,5 @@ class Form:
 
 	def show_buttons(self):
 		for b in self.buttons:
-			Button(self.main_frame, text=b.name, command=b.command).pack()
+			Button(self.form_frame, text=b.name, command=b.command,
+			       height=1, bg="#A877BA", font=('Arial', 16, 'bold'), pady=5).pack(padx=2, pady=10)
